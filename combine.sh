1fc59dd07e44e622634a4b437708b38dff31a0f0
Marcelo Salazar <marsal01@e109747-mac.cambridge.arm.com>
2018-02-13 17:51:08 +0000

    Remove bootloader

--- combine.sh
+++
@@ -1,1 +0,0 @@
-python combine_bootloader_with_app.py -b mbed-bootloader-k64f.bin -a BUILD/K64F/GCC_ARM/simple-mbed-cloud-client-example.bin --app-offset 0x20400 --header-offset 0x20000 -o combined.bin
